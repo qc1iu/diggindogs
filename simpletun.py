c022e6ab4da7e5efb3ccb115df9655bbd53c9f93
qc1iu <qc1iu@vip.qq.com>
2017-08-12 15:35:02 +0800

    new

--- simpletun.py
+++ simpletun.py
@@ -1,27 +1,35 @@
-# /usr/bin/env python3
+#! /usr/bin/env python3
+
+import argparse
 import select
-import threading
-import termios
 import os
 import struct
 import fcntl
 import binascii
 import sys
+import socket
 
 TUNSETIFF = 0x400454ca
 IFF_TAP = 0x0002
 IFF_TUN = 0x0001
 IFF_NO_PI = 0x1000
 BUFSIZE = 2000
-CLIENT = 0
-SERVER = 1
-remote_ip = ''
-PORT = 55555
 
 # some common lengths
 IP_HDR_LEN = 20
 ETH_HDR_LEN = 14
 ARP_PKT_LEN = 28
+
+
+def do_args():
+    parser = argparse.ArgumentParser(description="simpletun!!")
+    parser.add_argument('-i', '--ifacename', type=str, default="tun0")
+    parser.add_argument('-s', '--server', action="store_true")
+    parser.add_argument('-c', '--client')
+    parser.add_argument('-p', '--port', type=int, default=55555)
+    parser.add_argument('-d', '--debug', action="store_true")
+
+    return parser.parse_args()
 
 
 def ERROR(*args, **kw):
@@ -66,32 +74,33 @@ def recv_thread(f, t):
         t.sendall(data)
 
 
-def doit():
+def doit(is_server, remote_ip, debug, port=55555, ifacename="tun0"):
     net_fd = 0
     net_socket = 0
-    tun_fd = create_tun("tun0", IFF_TUN | IFF_NO_PI)
+    tun_fd = create_tun(ifacename, IFF_TUN | IFF_NO_PI)
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except:
-        ERROR("Failed to create socket")
+    except all as e:
+        ERROR("Failed to create socket %s" % e)
 
     s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # client
-    if cliserv == CLIENT:
+    if is_server == False:
         try:
-            s.connect((remote_ip, PORT))
+            s.connect((remote_ip, port))
         except:
             ERROR("Client connect to %s error" % remote_ip)
         net_socket = s
     # server
     else:
         try:
-            s.bind(('127.0.0.1', PORT))
+            s.bind(('127.0.0.1', port))
         except:
             ERROR("Bind failed.")
 
-        s.lisent()
+        s.listen()
 
         conn, remote_addr = s.accept()
         LOG("[Server] Connected with "
@@ -99,33 +108,43 @@ def doit():
         net_socket = conn
 
     net_fd = net_socket.fileno()
-    rlist = [net_fd tun_fd]
+    rlist = [net_fd, tun_fd]
     while True:
-        r_list, w_list, x_list = select(rlist, None, None, None)
+        r_list, w_list, x_list = select.select(rlist, [], [], None)
 
         for r in r_list:
             if r is net_fd:
-
                 plength = os.read(r, 4)
                 plength = struct.unpack("i", plength)
+                if plength == 0:
+                    # ctrl-c
+                    break
                 data = os.read(r, plength)
-
+                DEBUG("Read %d bytes from the network\n" % len(data))
                 nwrite = os.write(tun_fd, data)
                 DEBUG("Written %d bytes to the tap interface" % nwrite)
             else:
                 # write length
                 data = os.read(r, BUFSIZE)
                 nread = len(data)
-                DEBUG("Read %d bytes from the tap interface\n" % nread)
+                DEBUG("Read %d bytes from the tap interface" % nread)
 
                 plength = struct.pack("i", nread)
                 os.write(net_fd, plength)
                 nwrite = os.write(net_fd, data)
 
-                DEBUG("Written %d bytes to the network\n",  nwrite)
+                DEBUG("Written %d bytes to the network"  % nwrite)
 
     os.close(tun_fd)
 
 
 if __name__ == "__main__":
-    doit()
+    args = do_args()
+    print(args)
+    # check arg
+    if args.server and args.client != None:
+        raise Exception("wrong args")
+    if args.server == False and args.client == None:
+        raise Exception("wrong args")
+
+    doit(args.server, args.client, args.debug, args.port, args.ifacename)
